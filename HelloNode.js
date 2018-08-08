ec6198d8295bf3437ed2fff13a2531f2f8b696a4
theipol <thepolivera@gmail.com>
2018-08-08 18:54:03 -0300

    Se agrego 2018

--- HelloNode.js
+++ HelloNode.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hola Mundo!');
+  res.send('Hola Mundo 2018!');
 });
 app.listen(3000, function () {
   console.log('Ejemplo escuchando en puerto 3000!');
